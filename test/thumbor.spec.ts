f8dd6c6d016e320205c7ad9ebf28c6a5bd61e1f6
brettm12345 <brettmandler@gmail.com>
2020-02-02 22:34:23 -0500

    Switch to snapshot testing

--- test/thumbor.spec.ts
+++ test/thumbor.spec.ts
@@ -1,13 +1,13 @@
 import Thumbor from '../src';
 
 describe('thumbor', () => {
-  const serverUrl = 'http://localhost';
-  const unsafeUrl = `${serverUrl}/unsafe`;
-  const imagePath = 'react-day-picker.png';
-  const image = Thumbor({ serverUrl, imagePath });
+  const image = Thumbor({
+    serverUrl: 'http://localhost',
+    imagePath: 'react-day-picker.png'
+  });
 
   it('Generates urls', () => {
-    expect(image.buildUrl()).toBe(`${unsafeUrl}/${imagePath}`);
+    expect(image.buildUrl()).toMatchSnapshot();
   });
 
   it('Removes duplicates', () => {
@@ -17,33 +17,29 @@ describe('thumbor', () => {
         .grayscale()
         .grayscale()
         .buildUrl()
-    ).toBe(`${unsafeUrl}/filters:grayscale()/${imagePath}`);
+    ).toMatchSnapshot();
   });
 
   it('Works with resize', () => {
-    expect(image.resize(30, 30).buildUrl()).toContain('30x30');
+    expect(image.resize(30, 30).buildUrl()).toMatchSnapshot();
   });
 
   it('Works with crop', () => {
-    expect(image.crop(10, 10, 30, 30).buildUrl()).toContain(
-      '10x10:30x30'
-    );
+    expect(image.crop(10, 10, 30, 30).buildUrl()).toMatchSnapshot();
   });
 
   it('Works with filter', () => {
-    expect(image.quality(80).buildUrl()).toContain('quality(80)');
+    expect(image.quality(80).buildUrl()).toMatchSnapshot();
   });
 
   it('Works with many filters', () => {
-    ['quality(80)', 'format(webp)', 'autoJpg()'].forEach(
-      expect(
-        image
-          .quality(80)
-          .autoJpg()
-          .format('webp')
-          .buildUrl()
-      ).toContain
-    );
+    expect(
+      image
+        .quality(80)
+        .autoJpg()
+        .format('webp')
+        .buildUrl()
+    ).toMatchSnapshot();
   });
 
   it('Works with security keys', () => {
